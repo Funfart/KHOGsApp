0ba4c040a6a9747f68da525c0416e7d5f51361f3
Funfart Games <funfartgames@gmail.com>
2026-04-17 01:04:31 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Character from './components/Character';
 import Navigation from './components/Navigation';
 import NFTCard from './components/NFTCard';
 import { connectWallet, reconnectWallet, handleMobileWalletRedirect } from './lib/wallet';
-import { fetchNFTs } from './lib/opensea';
+import { fetchNFTs } from './pages/api/opensea';
 
 export default function Page() {
   const tabsRef = useRef([]);
