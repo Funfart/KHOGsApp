cd1502825539aad9374a3b4a27b6a75b563fcdf7
Funfart Games <funfartgames@gmail.com>
2026-04-18 02:52:06 -0400

    Update NPC.jsx

--- app/components/NPC.jsx
+++ app/components/NPC.jsx
@@ -1,54 +1,38 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
-
-export default function NPC({ data, onExit }) {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    const el = ref.current;
-    if (!el) return;
-
-    const startX = data.direction === 'right' ? -200 : 2760;
-    const endX = data.direction === 'right' ? 2760 : -200;
-
-    el.style.transform = `
-      translateX(${startX}px)
-      translateY(-50%)
-      scale(${data.scale})
-    `;
-
-    requestAnimationFrame(() => {
-      el.style.transition = `transform ${data.duration}ms linear`;
-
-      el.style.transform = `
-        translateX(${endX}px)
-        translateY(-50%)
-        scale(${data.scale})
-      `;
-    });
-
-    const timeout = setTimeout(() => {
-      onExit(data.id);
-    }, data.duration);
-
-    return () => clearTimeout(timeout);
-  }, [data, onExit]);
-
+export default function NPC({ data }) {
   return (
-    <img
-      ref={ref}
-      src={data.src}
-      className="npc"
+    <div
       style={{
-        top: `${data.y}%`,
         position: 'absolute',
+        bottom: 0,
+        left: 0,
         pointerEvents: 'none',
-        zIndex: data.scale > 0.9 ? 6 : 4,
-        animation: 'npcBounce 0.6s infinite ease-in-out'
+        zIndex: data.z,
+        transform: `translateX(${data.x}px)`
       }}
-      alt="npc"
-      draggable={false}
-    />
+    >
+      <div
+        style={{
+          animation: `npcBounce 0.35s infinite ease-in-out`
+        }}
+      >
+        <img
+          src={data.src}
+          alt="npc"
+          draggable={false}
+          style={{
+            width: '500px',
+
+            transform:
+              data.direction === 'right'
+                ? 'scaleX(-1)'
+                : 'scaleX(1)',
+
+            transformOrigin: 'bottom center'
+          }}
+        />
+      </div>
+    </div>
   );
 }
